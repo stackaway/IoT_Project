148f1e3cd589f5e96bd8f2178f1014330c196619
Churnika Mundas <churnika.mundas@gmail.com>
2024-03-17 23:38:52 +0530

    speech does not work!!!

--- Model/speech.py
+++ Model/speech.py
@@ -4,24 +4,29 @@ import speech_recognition as sr
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
 
-# Reading Microphone as source
-# listening the speech and store in audio_text variable
-with sr.Microphone() as source:
-    print("Talk")
-    audio_text = r.listen(source)
-    print("Time over, thanks")
+# Open the CSV file
+with open('/Users/churnika/Desktop/Projects/IoT_Project/Dataset/speech_to_text.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
 
-# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
-try:
-    # using google speech recognition
-    print("Text: "+r.recognize_google(audio_text))
-    text = r.recognize_google(audio_text)
+    # Start an infinite loop
+    while True:
+        # Reading Microphone as source
+        # listening the speech and store in audio_text variable
+        with sr.Microphone() as source:
+            print("Talk")
+            audio_text = r.listen(source, timeout=10, phrase_time_limit=10)
+            print("Time over, thanks")
 
-    # writing to csv file
-    with open('/Users/churnika/Desktop/Projects/IoT_Project/Dataset/speech_to_text.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([text])
+        # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+        try:
+            # using google speech recognition
+            text = r.recognize_google(audio_text)
+            print("Text: "+text)
 
-except:
-    print("Sorry, I did not get that")
+            # writing to csv file
+            writer.writerow([text])
+
+        except:
+            print("Sorry, I did not get that")
+
 
