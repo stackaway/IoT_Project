7fa8552f85e2fade7d5a6d4909149d007815f11e
Churnika Mundas <churnika.mundas@gmail.com>
2024-04-13 15:28:42 +0530

    working model

--- Model/speech.py
+++ Model/speech.py
@@ -17,16 +17,27 @@ with open('/Users/churnika/Desktop/Projects/IoT_Project/Dataset/speech_to_text.c
             audio_text = r.listen(source, timeout=10, phrase_time_limit=10)
             print("Time over, thanks")
 
-        # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+        # recognize_() method will throw a request error if the API is unreachable, hence using exception handling
         try:
             # using google speech recognition
             text = r.recognize_google(audio_text)
-            print("Text: "+text)
+            print("Text: " + text)
 
             # writing to csv file
             writer.writerow([text])
 
-        except:
-            print("Sorry, I did not get that")
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
 
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
+        except KeyboardInterrupt:
+            print("Interrupted by user")
+            break
+
+        except Exception as e:
+            print("An error occurred: {0}".format(e))
+
+    # Close the CSV file
+    file.close()
